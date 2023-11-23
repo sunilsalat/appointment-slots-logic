5ee42969011d7aca77563b303715bc65a7c5a2d9
Sunil Salat <sunil.salat@reynasolutions.com>
2023-11-23 15:56:07 +0530

    helper functions created

--- src/controllers/OderController.ts
+++ src/controllers/OderController.ts
@@ -2,6 +2,7 @@ import { Request, Response, request } from "express";
 import { User } from "../models/User";
 import { Order } from "../models/Order";
 import moment from "moment";
+import { generateSlots } from "../helper/generateSlotsBtwnTime";
 
 export const createOrder = async (req: Request, res: Response) => {
     const payload = req.body;
@@ -20,7 +21,6 @@ export const updateOrder = async (req: Request, res: Response) => {
 
 export const checkAvailability = async (req: Request, res: Response) => {
     let { providerId, requestDate } = req.body;
-    const availableSlots = [];
     requestDate = moment(requestDate, "YYYY-MM-DD");
     const dayOfWeek = requestDate.day();
 
@@ -41,42 +41,16 @@ export const checkAvailability = async (req: Request, res: Response) => {
     const todaysProviderWorkTime: any = provider?.workSchedule.find(
         (item) => item.day_of_week == dayOfWeek
     );
+    const { start_time, end_time, break_start_time, break_end_time } =
+        todaysProviderWorkTime;
 
     // generate slots based on providers standard availability
-    if (todaysProviderWorkTime) {
-        let initialStartTime = moment(
-            todaysProviderWorkTime.start_time,
-            "HH:mm:ss"
-        );
-
-        let breakStartTime = moment(
-            todaysProviderWorkTime.break_start_time,
-            "HH:mm:ss"
-        );
-
-        let breakEndTime = moment(
-            todaysProviderWorkTime.break_end_time,
-            "HH:mm:ss"
-        );
-
-        let endTime = moment(todaysProviderWorkTime.end_time, "HH:mm:ss");
-        let ST: any = initialStartTime;
-        let ET: any;
-        while (ST.isBefore(endTime)) {
-            ET = moment(ST, "HH:mm:ss").add(1, "hours");
-            if (
-                (ST.isBefore(breakStartTime) && ET.isBefore(breakStartTime)) ||
-                ST.isAfter(breakEndTime)
-            ) {
-                const obj = {
-                    startTime: ST,
-                    endTime: ET,
-                };
-                availableSlots.push(obj);
-            }
-            ST = moment(ST, "HH:mm:ss").add(1, "hours");
-        }
-    }
+    const availableSlots = generateSlots({
+        start_time,
+        end_time,
+        break_start_time,
+        break_end_time,
+    });
 
     res.status(200).json({ availableSlots });
 };
---
+++ src/helper/generateSlotsBtwnTime.ts
@@ -0,0 +1,38 @@
+import moment from "moment";
+
+export const generateSlots = ({
+    start_time,
+    end_time,
+    break_start_time,
+    break_end_time,
+}: any) => {
+    let availableSlots = [];
+
+    if (start_time) {
+        let initialStartTime = moment(start_time, "HH:mm:ss");
+
+        let breakStartTime = moment(break_start_time, "HH:mm:ss");
+
+        let breakEndTime = moment(break_end_time, "HH:mm:ss");
+
+        let endTime = moment(end_time, "HH:mm:ss");
+        let ST: any = initialStartTime;
+        let ET: any;
+        while (ST.isBefore(endTime)) {
+            ET = moment(ST, "HH:mm:ss").add(1, "hours");
+            if (
+                (ST.isBefore(breakStartTime) && ET.isBefore(breakStartTime)) ||
+                ST.isAfter(breakEndTime)
+            ) {
+                const obj = {
+                    startTime: ST,
+                    endTime: ET,
+                };
+                availableSlots.push(obj);
+            }
+            ST = moment(ST, "HH:mm:ss").add(1, "hours");
+        }
+
+        return availableSlots;
+    }
+};
